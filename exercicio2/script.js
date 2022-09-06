e9a83eb33df1dbd97048ff4e10be2498e7f85efe
Aline Carvalho <alineoak9@gmail.com>
2022-09-06 16:03:27 -0300

    Lista de exercicio A012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qual é a sua idade?");
+let terminouEnsinoMedio = confirm("Você já terminou  ensino médio?");
+let cursandoFaculdade = confirm("Voê já está cursando uma faculdade");
 
-if(){
-
+if(idade >=18){
+    console.log("Você atigigiu a maior idade.");
 }else{
-	
+	console.log("Você é menor de idade.");
+}
+
+if(terminouEnsinoMedio){
+    console.log("Você já terminou o ensino médio.");
+}else{
+    console.log("Você não terminou o ensino médio.");
+}
+
+if (cursandoFaculdade) {    
+    console.log("Você já está cursando a Faculdade");    
+}else{
+    console.log("Você não está cursando a Faculdade");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt("Qual é a sua nacionalidade: ").toLowerCase();
+
+     
+
+if (nacionalidade === "brasileira"){
+    console.log("Sua nacionalidade brasileira");
+}else if(nacionalidade === "argentina"){
+    console.log("Sua nacionalidade argentina");
+}else if (nacionalidade === "uruguaia") {
+    console.log("Sua nacionalidade uruguaia");
+} else if (nacionalidade === "chilena" ) {
+    console.log(nacionalidade === "colombiana");
+} else {
+    console.log("Nacionalidade não encontrada.");
+}
